9c85ec5d00bc941fce2b97e6d22a0627e80437b8
SandraSuresh-0701 <sandrasuresh0701@gmail.com>
2024-05-17 19:30:38 +0530

    add

--- src/axiosInterceptor.js
+++ src/axiosInterceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: 'https://mernback-bcvn.onrender.com', 
 });
 
 axiosInstance.interceptors.request.use(
